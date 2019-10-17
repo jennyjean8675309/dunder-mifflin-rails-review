bd8a2d86e0eec2c9a6e6dd144a74c5101a86d3af
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-10-17 17:25:32 -0400

    completed Dunder Mifflin pets lab

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,44 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(employee_params)
+        dog = Dog.find(params[:employee][:dog_id])
+        
+        if @employee.save
+            @employee.dog = dog
+            redirect_to employee_path(@employee)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        if @employee.update(employee_params)
+            redirect_to employee_path(@employee)
+        else 
+            render :edit
+        end
+    end
+
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :office, :title, :img_url, :dog_id)
+    end
 end
